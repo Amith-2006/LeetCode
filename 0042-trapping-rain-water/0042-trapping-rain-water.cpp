9d760dedb23e66c080757dadabea9b6daff3bc40
Amith Kumar K <amiththor018@gmail.com>
2026-08-03 21:47:02 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.88%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if(height.empty())
+             return 0;
+
+        int l=0;
+        int r=height.size()-1;
+        int leftmax=height[l],rightmax=height[r];
+        int res=0;
+        while(l<r){
+            if(leftmax<rightmax){
+                l++;
+                leftmax=max(leftmax,height[l]);
+                res+=leftmax-height[l]; 
+                /* Since we are checking every index individually we consider it as unit width*/
+            }
+            else{
+                r--;
+                rightmax=max(rightmax,height[r]);
+                res+=rightmax-height[r];
+            }
+        }
+        return res;
+    }
+};  
