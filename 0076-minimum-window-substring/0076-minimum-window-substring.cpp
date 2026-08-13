d06ccf7eba15bfacfc7974f355360f427f877991
Amith Kumar K <amiththor018@gmail.com>
2026-08-13 23:12:12 +0530

    Time: 0 ms (100%), Space: 11.6 MB (43.4%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,50 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int> mp;
-        unordered_map<char,int> window;
+        vector<int> need(128, 0);
+        vector<int> window(128, 0);
 
         for(char c : t) {
-            mp[c]++;
+            need[c]++;
         }
 
         int l = 0;
-        int r = 0;
-
         int have = 0;
-        int min_size = INT_MAX;
+        int minSize = INT_MAX;
         int start = 0;
 
-        while(r < s.size()) {
+        for(int r = 0; r < s.size(); r++) {
 
             window[s[r]]++;
 
-            if(mp.count(s[r]) && window[s[r]] <= mp[s[r]]) {
+            if(window[s[r]] <= need[s[r]]) {
                 have++;
             }
 
             while(have == t.size()) {
 
-                if(r - l + 1 < min_size) {
-                    min_size = r - l + 1;
+                if(r - l + 1 < minSize) {
+                    minSize = r - l + 1;
                     start = l;
                 }
 
                 window[s[l]]--;
 
-                if(mp.count(s[l]) && window[s[l]] < mp[s[l]]) {
+                if(window[s[l]] < need[s[l]]) {
                     have--;
                 }
 
                 l++;
             }
-
-            r++;
         }
 
-        if(min_size == INT_MAX)
+        if(minSize == INT_MAX) {
             return "";
+        }
 
-        return s.substr(start, min_size);
+        return s.substr(start, minSize);
     }
 };
