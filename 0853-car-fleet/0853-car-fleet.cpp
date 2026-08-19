83317023f09bad020db3c4ffc51cf0f2440cb66f
Amith Kumar K <amiththor018@gmail.com>
2026-08-19 22:26:56 +0530

    Time: 64 ms (27.39%), Space: 107.4 MB (46.45%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -3,28 +3,25 @@ public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n = position.size();
         vector<pair<int,int>> pair;
-        pair.reserve(n);
-
+        
         for(int i=0;i<n;i++){
             pair.push_back({position[i],speed[i]});
         }
 
         sort(pair.rbegin(),pair.rend());
 
-        vector<double> st;  // we use this vector as stack
-        st.reserve(n);      
-        // reserves n spaces which avoids repeated reallocations while inserting pairs
+        double last = 0;
+        int fleets = 0;
 
         for(auto p : pair){
             double time = (double)(target-p.first)/p.second;
-            st.push_back(time);
 
-            if(st.size()>=2 && st.back()<=st[st.size()-2]){  
-                //since u cant access 2nd element from top using stack , we used a vector
-                st.pop_back();
+            if(time > last){
+                fleets++;
+                last = time;
             }
         }
 
-        return st.size();
+        return fleets;
     }
 };
