9f50fc44f4f7d57ce3806dbf6a11b9b03078a114
Amith Kumar K <amiththor018@gmail.com>
2026-08-20 21:46:58 +0530

    Time: 0 ms (100%), Space: 134.3 MB (89.4%) - LeetHub

---
+++ 1776-car-fleet-ii/1776-car-fleet-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<double> getCollisionTimes(vector<vector<int>>& cars) {
+        int n = cars.size();
+        vector<double> res(n, -1);
+        vector<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty()) {
+                int j = st.back();
+
+                if (cars[i][1] <= cars[j][1]) {
+                    st.pop_back();
+                    continue;
+                }
+
+                double t = (double)(cars[j][0] - cars[i][0]) /
+                           (cars[i][1] - cars[j][1]);
+
+                if (res[j] == -1 || t < res[j]) {
+                    res[i] = t;
+                    break;
+                }
+
+                st.pop_back();
+            }
+
+            st.push_back(i);
+        }
+
+        return res;
+    }
+};
