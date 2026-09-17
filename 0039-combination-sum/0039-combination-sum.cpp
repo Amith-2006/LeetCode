4473364132523f78e7f34290f21b8ab1a5b87c6b
Amith Kumar K <amiththor018@gmail.com>
2026-09-17 21:36:42 +0530

    Time: 0 ms (100%), Space: 14.2 MB (57.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -3,25 +3,32 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> ds;
-        findCombinations(0,target,candidates,ans,ds);
+
+        sort(candidates.begin(), candidates.end());
+
+        findCombinations(0, target, candidates, ans, ds);
+
         return ans;
-        
     }
 
-    void findCombinations(int index, int target,vector<int> arr, vector<vector<int>> &ans, vector <int> ds){
-        if(index==arr.size()){
-            if(target==0){
-                ans.push_back(ds);
-            }
+    void findCombinations(int index, int target, vector<int>& arr,
+                          vector<vector<int>>& ans, vector<int>& ds) {
+
+        if(target == 0){
+            ans.push_back(ds);
             return;
         }
-        
 
-        if(arr[index]<=target){
-            ds.push_back(arr[index]);
-            findCombinations(index,target-arr[index],arr,ans,ds);
+        for(int i = index; i < arr.size(); i++){
+
+            if(arr[i] > target)
+                break;
+
+            ds.push_back(arr[i]);
+
+            findCombinations(i, target - arr[i], arr, ans, ds);
+
             ds.pop_back();
         }
-        findCombinations(index+1,target,arr,ans,ds);
     }
 };
